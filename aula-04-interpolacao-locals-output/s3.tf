1b51fcc2535f0e918ee7cc1e25301895abca4ce5
douglasmofet <contato@douglasmofet.com.br>
2023-01-16 21:50:12 -0300

    Using outuput values

--- aula-04-interpolacao-locals-output/s3.tf
+++ aula-04-interpolacao-locals-output/s3.tf
@@ -4,7 +4,7 @@ resource "aws_s3_bucket" "this" {
   tags = local.common_tags
 }
 
-resource "aws_s3_bucket_object" "this" {
+resource "aws_s3_object" "this" {
   bucket = aws_s3_bucket.this.bucket
   key    = "config/${local.ip_filepath}"
   source = local.ip_filepath
