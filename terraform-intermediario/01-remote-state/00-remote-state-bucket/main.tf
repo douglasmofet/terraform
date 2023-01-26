eb2c7469ca5b0fe91d8c3467cc30d749adb68fee
douglasmofet <contato@douglasmofet.com.br>
2023-01-25 21:38:46 -0300

    Initial configuration remote state

---
+++ terraform-intermediario/01-remote-state/00-remote-state-bucket/main.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_version = "1.3.5"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.46.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = var.aws_region
+  profile = var.aws_profile
+}
+
+//'pull'
+data "aws_caller_identity" "current" {
+}
+
+//'push'
+resource "aws_s3_bucket" "remote-state" {
+  bucket = "tfstate-${data.aws_caller_identity.current.account_id}"
+
+  tags = {
+    Description = "Stores terraform remote state file"
+    ManagedBy   = "Terraform"
+    Owner       = "Mofet"
+    CreateAt    = "24/01/2023"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.remote-state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+output "remote_state_bucket" {
+  value = aws_s3_bucket.remote-state.bucket
+}
+
+output "remote_state_bucket_arn" {
+  value = aws_s3_bucket.remote-state.arn
+}
