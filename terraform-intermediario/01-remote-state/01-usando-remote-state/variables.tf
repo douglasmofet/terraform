75ae4335581f11069b3389ce4c5cc1a2bb0ce327
douglasmofet <contato@douglasmofet.com.br>
2023-01-25 22:06:24 -0300

    Work initial with remote state

---
+++ terraform-intermediario/01-remote-state/01-usando-remote-state/variables.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+  type        = string
+  description = ""
+  default     = "sa-east-1"
+}
+
+variable "aws_profile" {
+  type        = string
+  description = ""
+  default     = "tf01"
+}
+
+variable "ami" {
+  type          = string
+  description   = ""
+  default       = "ami-0b0d54b52c62864d6"
+}
+
+variable "instance_type" {
+  type        = string
+  description = ""
+  default     = "t1.micro"
+}
