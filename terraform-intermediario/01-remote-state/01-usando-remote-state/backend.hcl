438f0c98581ecd36bc2d72bdd73bba8069a71f14
douglasmofet <contato@douglasmofet.com.br>
2023-01-25 22:32:11 -0300

    Using a backend file

---
+++ terraform-intermediario/01-remote-state/01-usando-remote-state/backend.hcl
@@ -0,0 +1,4 @@
+    bucket  = "tfstate-671128741622"
+    key     = "dev/01-usando-remote-state/terraform.tfstate"
+    region  = "sa-east-1"
+    profile = "tf01"
--- terraform-intermediario/01-remote-state/01-usando-remote-state/main.tf
+++ terraform-intermediario/01-remote-state/01-usando-remote-state/main.tf
@@ -8,12 +8,7 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket  = "tfstate-671128741622"
-    key     = "dev/01-usando-remote-state/terraform.tfstate"
-    region  = "sa-east-1"
-    profile = "tf01"
-  }
+  backend "s3" {}
 }
 
 
